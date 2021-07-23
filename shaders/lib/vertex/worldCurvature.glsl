9c16de3cb319a2fde7a59d6e38f33a38bcd3a7b1
FlatJim <luke0marchant@gmail.com>
2021-07-23 14:42:47 +1000

    more curvature options

--- shaders/lib/vertex/worldCurvature.glsl
+++ shaders/lib/vertex/worldCurvature.glsl
@@ -1,4 +1,4 @@
-#define WorldCurvatureSize 256 //[16 32 64 128 256 512 1024 2048 4096]
+#define WorldCurvatureSize 256 //[-4096 -2048 -1024 -512 -256 -128 -64 -32 -16 -8 -4 -2 2 4 8 16 32 64 128 256 512 1024 2048 4096]
 
 float worldCurvature(vec2 pos){
     return dot(pos,pos)/WorldCurvatureSize;
